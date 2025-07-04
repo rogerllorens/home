cfe8a4860fcce8dad0bbfdd74fae2fbeb6c66c19
rogerllorens <rogerlv98@gmail.com>
2025-07-04 14:19:12 +0200

    Fix NDK patch for Kotlin DSL

--- scripts/patch_app_ndk.sh
+++ scripts/patch_app_ndk.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
 for file in android/app/build.gradle android/app/build.gradle.kts; do
-  if [ -f "$file" ]; then
-    if grep -q ndkVersion "$file"; then
-      sed -i "s/ndkVersion .*/ndkVersion '27.0.12077973'/" "$file"
-    else
-      sed -i "/android {/a\    ndkVersion '27.0.12077973'" "$file"
-    fi
-    echo "Ensured ndkVersion in $file"
+  [ -f "$file" ] || continue
+  if [[ $file == *.kts ]]; then
+    replace="ndkVersion = \"27.0.12077973\""
+  else
+    replace="ndkVersion '27.0.12077973'"
   fi
+  if grep -q ndkVersion "$file"; then
+    sed -i "s/ndkVersion.*/$replace/" "$file"
+  else
+    sed -i "/android {/a\    $replace" "$file"
+  fi
+  echo "Ensured ndkVersion in $file"
 done
