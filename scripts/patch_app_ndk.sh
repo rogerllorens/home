f53f159e4c85be091ba1209847e5546248d071e5
rogerllorens <rogerlv98@gmail.com>
2025-07-04 14:19:21 +0200

    Fix NDK version quoting

--- scripts/patch_app_ndk.sh
+++ scripts/patch_app_ndk.sh
@@ -3,12 +3,12 @@ set -e
 for file in android/app/build.gradle android/app/build.gradle.kts; do
   [ -f "$file" ] || continue
   if [[ $file == *.kts ]]; then
-    replace="ndkVersion = \"27.0.12077973\""
+    replace='ndkVersion = "27.0.12077973"'
   else
     replace="ndkVersion '27.0.12077973'"
   fi
   if grep -q ndkVersion "$file"; then
-    sed -i "s/ndkVersion.*/$replace/" "$file"
+    sed -i -E "s/ndkVersion[^\n]*/$replace/" "$file"
   else
     sed -i "/android {/a\    $replace" "$file"
   fi
