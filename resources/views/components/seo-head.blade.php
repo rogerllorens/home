2799075e102be8aef2827b88999ab3e21f77dc33
rogerllorens <rogerlv98@gmail.com>
2025-12-25 22:12:40 +0100

    Enforce SEO meta limits and test video JSON-LD

--- resources/views/components/seo-head.blade.php
+++ resources/views/components/seo-head.blade.php
@@ -6,9 +6,18 @@
     'ogImage' => null,
 ])
 
-<title>{{ $title }}</title>
+@php
+    $titleLimit = (int) config('candidboys.seo.title_max', 70);
+    $descLimit = (int) config('candidboys.seo.desc_max', 160);
+    $resolvedTitle = \Illuminate\Support\Str::limit($title, $titleLimit, '');
+    $resolvedDescription = $description
+        ? \Illuminate\Support\Str::limit($description, $descLimit, '')
+        : null;
+@endphp
+
+<title>{{ $resolvedTitle }}</title>
 @if ($description)
-    <meta name="description" content="{{ $description }}">
+    <meta name="description" content="{{ $resolvedDescription }}">
 @endif
 @if ($canonical)
     <link rel="canonical" href="{{ $canonical }}">
